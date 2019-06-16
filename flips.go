5299de381d4179c9f406f464608b89e8ae0d1c5e
Stephen Wetzel <swetzel@inspireenergy.com>
2019-06-16 00:25:12 -0400

    Loop through different combinations of numbers of flips and goal streak lengths

--- flips.go
+++ flips.go
@@ -8,6 +8,7 @@ package main
 import "fmt"
 import "math/rand"
 import "time"
+import "flag"
 
 // Return an array of random flips, of the given length
 func create_flips(number_of_flips int) ([]int) {
@@ -29,6 +30,7 @@ func streak_found(flips []int, flip_number, streak_length int) (bool) {
 
 // Find a streak by picking random spots in our array of flips and then checking for a streak there.  Return as soon as we find one.
 func find_streak_random(flips []int, streak_length int) (int) {
+	if len(flips) <= streak_length { return -1 }
   tries := 2 * len(flips) + 10 // Since it's random we need to give up at some point.  This is purely my gut feeling for what is 'enough' tries.
   for try_number := 0; try_number < tries; try_number++ {
     flip_number := rand.Intn(len(flips) - streak_length)  // For a 100 flip array and 3 flip streak, this is 100 - 3 = 97 -> 0...96
@@ -39,24 +41,29 @@ func find_streak_random(flips []int, streak_length int) (int) {
   return -1 // This represents a failure to find any streaks of the desired length in this array of flips
 }
 
-
 func main() {
-  const STREAK_LENGTH = 1 // Number of 'heads' in a row we consider a 'streak'
-  const NUMBER_OF_FLIPS = 3 // How many times with flip our coin
-  const ROUNDS_TO_PERFORM = 100000 // A round consists of flipping a coin the desired times, finding 1 streak in it, and then checking the next flip after the streak.
+  max_streak_length := flag.Int("streak", 3, "Number of 'heads' in a row we consider a 'streak'")
+  max_number_of_flips := flag.Int("flips", 100, "How many times with flip our coin")
+  round_to_perform := flag.Int("rounds", 10000, "A round consists of flipping a coin the desired times, finding 1 streak in it, and then checking the next flip after the streak.")
+  flag.Parse()
 
-  count := 0.0
-  completed_rounds := 0.0 // A completed round is any where we are able to find a streak
+  for streak_length := 1; streak_length <= *max_streak_length; streak_length++ {
+  	for number_of_flips := 10; number_of_flips <= *max_number_of_flips; number_of_flips *= 10 {
 
-  rand.Seed(time.Now().UnixNano())
-  for round_num := 0; round_num < ROUNDS_TO_PERFORM; round_num++ {
-    flips := create_flips(NUMBER_OF_FLIPS) // Generate our random array of flips
-    streak_index := find_streak_random(flips, STREAK_LENGTH)
-    if streak_index < 0 { continue } // We failed to find a streak in this array of flips
-    streak_continued := flips[streak_index] == flips[streak_index + STREAK_LENGTH] // The key test
-    completed_rounds++
-    if streak_continued { count++ }
-  }
-  percent_continued_streak := (100.0 * count / completed_rounds)
-  fmt.Printf("Performed %v rounds, and %v were successful, found %.2f%% continued the streak.\n", ROUNDS_TO_PERFORM, completed_rounds, percent_continued_streak)
+		  count := 0
+		  completed_rounds := 0 // A completed round is any where we are able to find a streak
+
+		  rand.Seed(time.Now().UnixNano())
+		  for round_num := 0; round_num < *round_to_perform; round_num++ {
+		    flips := create_flips(number_of_flips) // Generate our random array of flips
+		    streak_index := find_streak_random(flips, streak_length)
+		    if streak_index < 0 { continue } // We failed to find a streak in this array of flips
+		    streak_continued := flips[streak_index] == flips[streak_index + streak_length] // The key test
+		    completed_rounds++
+		    if streak_continued { count++ }
+		  }
+		  percent_continued_streak := (100.0 * float64(count) / float64(completed_rounds))
+		  fmt.Printf("Looking for a streak of length %2d in %5d total flips. Performed %d rounds, and %6d were successful, found %.2f%% continued the streak.\n", streak_length, number_of_flips, *round_to_perform, completed_rounds, percent_continued_streak)
+		}
+	}
 }
